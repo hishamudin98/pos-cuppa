172a7b4b6a97525d8bf83998b449dd536825bfaf
Hishamudin98 <hishamudin.ali@gmail.com>
2022-10-03 07:48:39 +0800

    add loading logout

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ import App from './App';
 import {name as appName} from './app.json';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
+import { LogBox } from 'react-native';
+
+// Ignore log notification by message:
+LogBox.ignoreLogs(['Warning: ...']);
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
+
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
