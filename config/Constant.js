6e7aa9a66c1fbec3c062361cb2f3d6ce3ef31edd
Hishamudin98 <hishamudin.ali@gmail.com>
2022-10-25 00:46:01 +0800

    modify constant

--- config/Constant.js
+++ config/Constant.js
@@ -25,9 +25,11 @@ export const system_configuration = {
   ENVIRONMENT: 'development',
   APP_TITLE: 'Malaya Grill',
   APP_COMPANY: 'MALAYA EMPIRE SDN BHD',
-  counterSecretKey: '1234-1235',
+  counterSecretKey: '1234-1234', // sme bank
+  // REACT_APP_DEV_MODE: 'http://192.168.0.176:3000',
+  // REACT_APP_DEV_MODE: 'http://192.168.100.60:3000',
   // REACT_APP_DEV_MODE: 'http://localhost:3000',
-  REACT_APP_DEV_MODE: 'http://192.168.100.60:3000',
+  REACT_APP_DEV_MODE: 'https://stg-heandshe.toyyibfnb.com/api',
   REACT_APP_PROD_MODE: 'https://api-prod.application.com',
 };
 
